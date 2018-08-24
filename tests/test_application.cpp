eca64278776369700b448d599121f54df26efd9d
Emiel van Dam <emiel.van.dam@hetconsultancyhuis.nl>
2018-08-24 23:21:12 +0200

    changes

--- tests/test_application.cpp
+++ tests/test_application.cpp
@@ -1,10 +1,11 @@
+#include <util/logstream.hpp>
 #include <base/platform_application.hpp>
 #include <iostream>
 
 using namespace wee;
 
 int main(int argc, char* argv[]) {
-
+    DEBUG_METHOD();
     application* app = NULL;
     try {
         app = new platform_application;
