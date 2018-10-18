80976741195d8d1be52fd42e99c567a5af655d40
Emiel van Dam <emiel.van.dam@hetconsultancyhuis.nl>
2018-10-18 11:27:55 +0200

    migrated util -> core, now make some gameplay!

---
+++ src/core/logstream.cpp
@@ -0,0 +1,13 @@
+#include <core/logstream.hpp>
+
+using namespace wee;
+
+/*
+namespace std { 
+    wee::logstream & endl(wee::logstream & out) { 
+        out.put('\n'); 
+        out.flush(); 
+        return out; 
+    } 
+}
+*/
