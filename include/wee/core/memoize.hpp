c4170df6e7e356bbd4cf698cbb327d540dfa4b35
Emiel van Dam <emiel.van.dam@hetconsultancyhuis.nl>
2018-11-07 13:13:21 +0100

    memoize

---
+++ include/wee/core/memoize.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace wee {
+    template <typename R, typename... Args>
+    std::function<R (Args...)> memoize(R(*fn)(Args...)) {
+        std::unordered_map<std::tuple<Args...>, R> storage;
+        return [fn, storage] (Args... args) -> R 
+        {
+            auto arg = std::make_tuple(args...);
+            auto it = storage.find(arg);
+            if(it == storage.end()) {
+                // auto res = fn(args...);
+                // storage[arg] = res;
+                // return res;
+                storage.insert(std::make_pair(
+                    arg, fn(args...)
+                ).first->second;
+            } else {
+                return it->second;
+            }
+        }
+    }
+}
