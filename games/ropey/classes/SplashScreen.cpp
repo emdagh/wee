8106b114e608c6922210603c2c5704178b74a695
Emiel van Dam <emiel.van.dam@hetconsultancyhuis.nl>
2018-11-12 21:07:25 +0100

    should build now

--- games/ropey/classes/SplashScreen.cpp
+++ games/ropey/classes/SplashScreen.cpp
@@ -3,36 +3,21 @@
 using namespace wee;
 
 class splash_screen : public gamescreen {
-    std::vector<SDL_Texture*> _images;
 public:
-
-    virtual ~splash_screen() {
-    }
 };
 
 
 class mainmenu_screen : public gamescreen {
-    std::vector<SDL_Texture*> _images;
 public:
-
-    virtual ~splash_screen() {
-    }
 };
 
 class gameplay_screen : public gamescreen {
-    std::vector<SDL_Texture*> _images;
 public:
-
-    virtual ~splash_screen() {
-    }
 };
 
 
 class pause_screen : public gamescreen {
-    std::vector<SDL_Texture*> _images;
 public:
 
-    virtual ~splash_screen() {
-    }
 };
 
