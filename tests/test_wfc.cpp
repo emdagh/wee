20e4599dc335cce2266b84d5c44733abe3c319a7
Emiel van Dam <emiel.van.dam@hetconsultancyhuis.nl>
2018-12-14 15:40:07 +0100

    started my own version of the algortithm, I think I understand the basics quite well at the moment

--- tests/test_wfc.cpp
+++ tests/test_wfc.cpp
@@ -2,6 +2,7 @@
 #include <tuple>
 #include <cmath>
 #include <map>
+#include <unordered_map>
 #include <core/range.hpp>
 #include <core/random.hpp>
 #include <prettyprint.hpp>
@@ -73,8 +74,8 @@ public:
     }
 
     tensor(const array_type& data, const index_type& shape) 
-    : _data(data) 
-    , _shape(shape)
+    : _shape(shape)
+    , _data(data) 
     {
     }
 
@@ -142,24 +143,161 @@ private:
 struct int2 {
     int x, y;
 };
+/*
+struct WaveFunctionCollapse {
+    void run() {
+        patterns_from_sample();
+        build_propagator();
+        while(bool done = false; !done) {
+            observe();
+            propagate();
+        }
+        output_observations();
+    }
 
-void wfc(const int* in_map, const int2& in_size) {
+    void patterns_from_sample() {}
+    void build_propagator() {}
+    void observe() {
+        find_lowest_entropy();
+
+    }
+};
+*/
+
+template <typename T>
+auto to_bitmask = [] (size_t index) {
+    return static_cast<T>(1 << index);
+};
+
+template <typename bitmask_t>
+[[maybe_unused]] auto to_index = [] (const bitmask_t& b) {
+    bitmask_t bm = b;
+    unsigned r = 0;
+    while(bm >>= 1) r++;
+    return r;
+};
+
+template <typename It>
+auto random_from(It begin, It end) {
+    auto len = std::distance(begin, end);
+    auto rndi = static_cast<decltype(len)>(wee::randf(0.f, static_cast<float>(len)));
+    auto it = begin;
+    std::advance(it, rndi);
+    return it; 
+};
+
+void wfc(const int* in_map, const int2& in_size, int* , const int2& out_size) {
     /**
      * get all unique tiles + their frequencies
      */
-    typedef int64_t bitmask_t;
-    struct tile { bitmask_t sides[4]; };
+    typedef uint64_t bitmask_t;
+    constexpr int kNumDimensions = 2;
+    constexpr int kNumEdges = kNumDimensions << 1; 
+    struct tile { 
+        bitmask_t sides[kNumEdges]; 
+        int freq; 
+        size_t tidx;
+    };
+    
+    /**
+     * Step 1: read the map, and for each input tile; register it and count it's frequency.
+     */
     int n = in_size.x * in_size.y;
+    std::vector<int> tileset(in_map, in_map + n);
+    std::sort(tileset.begin(), tileset.end());
+    tileset.erase(std::unique(tileset.begin(), tileset.end()), tileset.end());
+
+    DEBUG_VALUE_OF(tileset);
+    
+    /*
+    std::array<int, 64> tile_lookup;
+    tile_lookup.fill(-1);
     std::vector<tile> tiles;
-    std::array<int, 256> tile_lookup; // << why 256???
     for(int i=0; i < n; i++) {
-        int t = in_map[i];
-        if(tile_lookup[t] < 0) {
-            tile_lookup[t] = static_cast<int>(tiles.size());
+        int ti = in_map[i];
+        if(tile_lookup[ti] < 0) {
+            tile_lookup[ti] = static_cast<int>(tiles.size());
             tiles.push_back(tile{});
+        }
+        tile& t = tiles[tile_lookup[ti]];
+        t.freq++;
+        t.tidx = ti;
+    }
+
+    DEBUG_VALUE_OF(tile_lookup);
+    */
+    /**
+     * Step 2: Construct the valid neigbor bitmasks
+     */
+    [[maybe_unused]] constexpr int2 neighbors[kNumEdges] = { 
+        { 0,  1}, // top
+        { 1,  0}, // right
+        { 0, -1}, // bottom
+        {-1,  0}, // left
+    };
+
+
+    //tensor<int, 2> tmap_in(std::valarray<int>(in_map, n), { (size_t)in_size.x, (size_t)in_size.y });
+    
+    /*for(int y=0; y < in_size.y; y++) {
+        for(int x=0; x < in_size.x; x++) {
+            int i = x + y * in_size.x;
+            tile& t = tiles[tile_lookup[in_map[i]]];
+            for(size_t j=0; j < kNumEdges; j++) {
+                const int2& n = neighbors[j];
+                int2 p;
+                p.x = (n.x + x + in_size.x) % in_size.x;
+                p.y = (n.y + y + in_size.y) % in_size.y;
+                int k = in_map[p.x + p.y * in_size.x];
+                assert(tile_lookup[k] >= 0);
+                t.sides[j] |= to_bitmask<bitmask_t>(tile_lookup[k]);
+            }
+        }
+    }
+    */
+
+    std::unordered_map<int, int> lookup;
+    for(size_t i=0; i < tileset.size(); i++) {
+        lookup[tileset[i]] = i;
+    }
+
+    DEBUG_VALUE_OF(lookup);
+
+    std::vector<bitmask_t> adjacency(tileset.size()); 
+    for(int y=0; y < in_size.y; y++) {
+        for(int x=0; x < in_size.x; x++) {
+            int self = in_map[x + y * in_size.x];
+
+            for(int z=0; z < kNumEdges; z++) {
+                const int2& n = neighbors[z];
+                int2 p = {
+                    (x + n.x + in_size.x) % in_size.x,
+                    (y + n.y + in_size.y) % in_size.y
+                };
+                int nt = in_map[p.x + p.y * in_size.x];
+                adjacency[lookup[self]] |= to_bitmask<bitmask_t>(lookup[nt]);
+            }
         }
     }
 
+    DEBUG_VALUE_OF(adjacency);
+
+    size_t out_n = out_size.x * out_size.y;
+    
+    std::vector<bitmask_t> cells(out_n);
+    std::generate(std::begin(cells), std::end(cells), [] () { return -1; }); // everything is possible...
+    
+    int cix = static_cast<int>(wee::randf(0, out_n) + 0.5f); // index of random cell on the N+1D grid.
+    [[maybe_unused]] bitmask_t& cell = cells[cix];
+    [[maybe_unused]] int rn = lookup[*random_from(tileset.begin(), tileset.end())];//[static_cast<int>(wee::randf(0, tileset.size() - 1))];
+    
+    cell = to_bitmask<bitmask_t>(rn); //pick a random value from the set of unique tiles.
+   
+
+
+
+    DEBUG_VALUE_OF(cells);
+    //tensor<int, 3> board(
 }
 
 
@@ -170,8 +308,22 @@ tensor<T, 1> arange(T t) {
     return res;
 }
 int main(int, char**) {
-    //[[maybe_unused]] tensor<float, 3> t(3, 3, 3);
-    auto t = arange(3 * 3 * 3).reshape(3, 3, 3);
+
+    int in_map[] = { 
+        820, 821, 820, 821,
+        820, 822, 822, 823,
+        822, 821, 820, 822,
+        822, 823, 822, 825
+    };
+
+    DEBUG_VALUE_OF(__builtin_ctzl(1UL << 33));
+
+    constexpr size_t kOutputDimension = 4;
+    int* out_map = new int[kOutputDimension * kOutputDimension];
+
+    wfc(in_map, { 4, 4 }, out_map, { kOutputDimension, kOutputDimension });
+
+    /*auto t = arange(3 * 3 * 3).reshape(3, 3, 3);
 
     auto va = t.values();
     DEBUG_VALUE_OF(va);
@@ -180,7 +332,7 @@ int main(int, char**) {
     // iterate through z-dimension at [1, 0]
     for(auto it = std::begin(ar); it != std::end(ar); it++) {
         DEBUG_VALUE_OF(*it);
-    }
+    }*/
 
     return 0;
 }
