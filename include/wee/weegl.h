d83d0a2db4c5301529bf282d70593ebbd884041f
Emiel van Dam <emiel.van.dam@hetconsultancyhuis.nl>
2019-06-18 01:14:27 +0200

    pong

--- include/wee/weegl.h
+++ include/wee/weegl.h
@@ -3,8 +3,8 @@
 #define GL_GLEXT_PROTOTYPES 1
 #if defined(_MSC_VER)
 # include <windows.h>
-# include <gl/GL.h>
+//# include <gl/GL.h>
 #endif
-//#include <SDL_opengl.h>
+#include <SDL_opengl.h>
 #include <SDL_opengl_glext.h>
 #define BUFFER_OFFSET(i) ((char *)NULL + (i)) 
