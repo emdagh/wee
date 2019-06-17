fa9c7c4712b1f02861546e14eb3a454ce0c3fa79
Emiel van Dam <emiel.van.dam@hetconsultancyhuis.nl>
2019-06-17 22:28:32 +0200

    missing files

---
+++ games/ropey/assets/levels/magicforest.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="tileset" tilewidth="32" tileheight="32" tilecount="1160" columns="29">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="../img/tileset.png" width="928" height="320"/>
+</tileset>
