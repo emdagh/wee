c69a858f8c436534b8f251dad61a510e74eb938a
Emiel van Dam <emiel.van.dam@hetconsultancyhuis.nl>
2019-06-18 23:10:13 +0200

    windows build should now export all symbols, also added export macros

--- include/wee/wee.h
+++ include/wee/wee.h
@@ -1,35 +1,18 @@
 #ifndef _WEE_H_
 #define _WEE_H_
 
+#include <wee/config.h>
+
 #if defined(_MSC_VER)
 #include <Windows.h>
 # define __aligned_type(x)  __declspec(align(x))
-# define __public_type      __declspec(dllexport)
-# define __private_type
-//# define __forceinline      __forceinline
-# define __noinline         __declspec(noinline)
 #elif defined(__GNUC__)
-
 # define __aligned_type(x)  __attribute__((aligned(x)))
-# define __public_type      __attribute__((visibility("default")))
-# define __private_type     __attribute__((visibility("hidden")))
-# define __forceinline      __attribute__((always_inline))
-# define __noinline         __attribute__((noinline))
-# define __debugbreak()
 #else
 # define __aligned_type(x)
-# define __public_type
-# define __private_type
 #endif
-
 #define __declare_aligned(T, n)     T __aligned_type(n)
-#define __export                    __public_type
 
-#if defined(UNIX)
-#define PATH_SEP "/"
-#else
-#define PATH SEP "\\"
-#endif
 
 
 #if defined(__cplusplus)
--- include/wee/wee.hpp
+++ include/wee/wee.hpp
@@ -2,13 +2,41 @@
 
 #include <wee/wee.h>
 #include <exception>
-#include <stdexcept>
+#include <stdexcept>   
+// Generic helper definitions for shared library support
+#if defined _WIN32 || defined __CYGWIN__
+#define WEE_HELPER_DLL_IMPORT __declspec(dllimport)
+#define WEE_HELPER_DLL_EXPORT __declspec(dllexport)
+#define WEE_HELPER_DLL_LOCAL
+#else
+#if __GNUC__ >= 4
+#define WEE_HELPER_DLL_IMPORT __attribute__ ((visibility ("default")))
+#define WEE_HELPER_DLL_EXPORT __attribute__ ((visibility ("default")))
+#define WEE_HELPER_DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+#else
+#define WEE_HELPER_DLL_IMPORT
+#define WEE_HELPER_DLL_EXPORT
+#define WEE_HELPER_DLL_LOCAL
+#endif
+#endif
+
+#ifdef WEE_LIB_SHARED // defined if wee is compiled as a shared object
+#ifdef WEE_LIB_EXPORTS // defined if we are building the wee shared object (instead of using it)
+#define WEE_API WEE_HELPER_DLL_EXPORT
+#else
+#define WEE_API WEE_HELPER_DLL_IMPORT
+#endif
+#define WEE_LOCAL WEE_HELPER_DLL_LOCAL
+#else 
+#define WEE_API
+#define WEE_LOCAL
+#endif // FOX_DLL
 
 #ifdef _MSC_VER
 #pragma warning (disable: 4146)
 #   define ALWAYS_INLINE __forceinline
 #elif defined(__GNUC__)
-#   define ALWAYS_INLINE __attribute__((always_inline)) inline
+# define ALWAYS_INLINE __attribute__((always_inline)) inline
 #else
 #   define ALWAYS_INLINE inline
 #endif
@@ -27,13 +55,13 @@ void operator=(TypeName) = delete
 
 namespace { // prevent cluttering of global namespace
 
-    struct not_implemented : std::logic_error {
+    struct WEE_API not_implemented : std::logic_error {
         not_implemented() : std::logic_error("function or method not implemented") {
 
         }
     };
 
-    struct file_not_found : std::runtime_error {
+    struct WEE_API file_not_found : std::runtime_error {
         file_not_found(const std::string& what) : std::runtime_error("file not found: " + what) {}
     };
 
