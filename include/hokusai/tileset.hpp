7b9384bb2bdeddaa775a8b3757c9fc54e0e3309d
Emiel van Dam <emiel.van.dam@hetconsultancyhuis.nl>
2019-06-20 22:49:19 +0200

    fixed a bug in tileset frequencies

--- include/hokusai/tileset.hpp
+++ include/hokusai/tileset.hpp
@@ -9,7 +9,63 @@ struct tileset {
      */
     std::vector<T> _data;
     std::unordered_map<T, size_t> _names;
-    std::vector<float> _frequency;
+    //std::vector<float> _frequency;
+    std::map<T, float> _frequency;
+
+
+    tileset() { 
+        push(0); 
+    }
+
+    T to_tile(size_t i) const { return _data[i]; }
+    size_t to_index(T t) const { return _names.at(t); }//_data.at(_names.at(t)); }
+    void set_frequency(T t, float f) { 
+
+        _frequency[t] = f;
+    }
+    /**
+     * returns the frequencies for each _index_
+     */
+    const std::vector<float> frequencies() const { 
+#if 0
+        std::vector<float> res(_names.size());
+        for(auto& kv: _names) {
+            res[kv.second] = _frequency.at(kv.first);
+        }
+        return res;
+#else
+        std::vector<float> res(_frequency.size());
+        for(auto& kv : _frequency) {
+            res[kv.first] = kv.second;
+        }
+        return res;
+#endif
+    }
+    size_t length() const { return _data.size(); }
+    
+    template <typename InputIt, typename OutputIt>
+    void make_weights(InputIt first, InputIt last, OutputIt d_first) const {
+        std::multiset<typename InputIt::value_type> temp(first, last);
+        for(auto it = temp.begin(); it != temp.end(); it = temp.upper_bound(*it)) {
+            auto ti = this->to_index(*it);
+            *(d_first + ti) = temp.count(*it);
+        }
+    }
+    
+    void push(T t) {
+        if(_names.count(t) == 0) {
+            _names.insert(std::make_pair(t, _data.size()));
+            _data.push_back(t);
+            _frequency[t] = 1;
+        } else {
+            _frequency[t]++;
+        }
+    }
+
+    template <typename OutputIt>
+    void weights(OutputIt d_first) const {
+        make_weights(_data.begin(), _data.end(), d_first);
+    }
 
     template <typename Iter>
     static tileset make_tileset(Iter first, Iter last) {
@@ -19,40 +75,5 @@ struct tileset {
             res.push(*ptr++);
         }
         return res;
-    }
-
-    tileset() { 
-        push(0); 
-    }
-
-    T to_tile(size_t i) const { return _data[i]; }
-    size_t to_index(T t) const { return _names.at(t); }//_data.at(_names.at(t)); }
-    void set_frequency(T t, float f) { _frequency[to_index(t)] = f; }
-    const std::vector<float> frequencies() const { return _frequency; }
-    size_t length() const { return _data.size(); }
-    
-    template <typename InputIt, typename OutputIt>
-    void make_weights(InputIt first, InputIt last, OutputIt d_first) const {
-        std::multiset<typename InputIt::value_type> temp(first, last);
-        for(auto it = temp.begin(); it != temp.end(); it = temp.upper_bound(*it)) {
-            auto ti = to_index(*it);
-            *(d_first + ti) = temp.count(*it);
-        }
-    }
-    
-    void push(T t) {
-        if(_names.count(t) == 0) {
-            _names.insert(std::make_pair(t, _data.size()));
-            _data.push_back(t);
-            _frequency.push_back(1);
-        } else {
-            size_t idx = to_index(t);
-            _frequency[idx]++;
-        }
-    }
-
-    template <typename OutputIt>
-    void weights(OutputIt d_first) const {
-        make_weights(_data.begin(), _data.end(), d_first);
     }
 };
