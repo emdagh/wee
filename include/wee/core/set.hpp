74339f243c30d7903b6304afc74df724228631af
Emiel van Dam <emiel.van.dam@hetconsultancyhuis.nl>
2020-05-02 15:35:48 +0200

    added set algos

---
+++ include/wee/core/set.hpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+
+namespace wee {
+    template <typename T>
+    T set_intersect(const T& a, const T& b) {
+        using key_type = typename T::value_type;
+        if(a.size() <= b.size()) {
+            T res;
+            std::copy_if(a.begin(), a.end(), std::inserter(res, res.end()), [&b] (const key_type& k) {
+                return b.count(k) != 0;
+            });
+            return res;
+        }
+        return set_intersect(b, a);
+    }
+    template <typename T>
+    T set_difference(const T& a, const T& b) {
+        using key_type = typename T::value_type;
+        if(a.size() <= b.size()) {
+            T res;
+            std::copy_if(a.begin(), a.end(), std::inserter(res, res.end()), [&b] (const key_type& k) {
+                return b.count(k) == 0;
+            });
+            return res;
+        }
+        return set_intersect(b, a);
+    }
+
+
+
+    template <typename T>
+    T set_union(const T& a, const T& b) {
+        T res { a };
+        res.insert(b.begin(), b.end());
+        return res;
+    
+    }
+}
