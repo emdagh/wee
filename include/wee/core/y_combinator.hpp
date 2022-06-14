d9b108e31facc94a72eb3486533e5dd290e32aee
emdagh <34509796+emdagh@users.noreply.github.com>
2022-06-14 15:41:53 +0200

    Create y_combinator.hpp

---
+++ include/wee/core/y_combinator.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+
+namespace wee
+{
+  template<class Fun>
+class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T>
+    explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+
+    template<class ...Args>
+    decltype(auto) operator()(Args &&...args) {
+        return fun_(std::ref(*this), std::forward<Args>(args)...);
+    }
+};
+
+template<class Fun>
+decltype(auto) y_combinator(Fun &&fun) {
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+
+/*int main(int, char**)
+{
+    auto almost_gcd = [](auto gcd, int a, int b) -> int {
+        return b == 0 ? a : gcd(b, a % b);
+    };
+    auto gcd = y_combinator(std::ref(almost_gcd));
+    std::cout << gcd(20, 30) << std::endl;
+    
+    return 0;
+}*/
+}
