f38e32be9afc46fbac326306d79eb9b402f89c30
emdagh <34509796+emdagh@users.noreply.github.com>
2022-06-15 11:03:13 +0200

    Update memoize.hpp

--- include/wee/core/memoize.hpp
+++ include/wee/core/memoize.hpp
@@ -1,23 +1,27 @@
 #pragma once
 
+#include "closure_traits.hpp"
+
 namespace wee {
-    template <typename R, typename... Args>
-    std::function<R (Args...)> memoize(R(*fn)(Args...)) {
-        std::unordered_map<std::tuple<Args...>, R> storage;
-        return [fn, storage] (Args... args) -> R 
+template <typename F>
+auto memoize(F fun)
+{
+    using key_t = typename closure_traits<F>::args;
+    using val_t = typename closure_traits<F>::return_type;
+
+    static std::unordered_map<key_t, val_t, hash<key_t>> cache;
+    
+    return [fun = std::forward<F>(fun)] (auto... args) 
+    {
+        auto key = std::tuple(args...);
+        if(auto it = cache.find(key); it != cache.end())
         {
-            auto arg = std::make_tuple(args...);
-            auto it = storage.find(arg);
-            if(it == storage.end()) {
-                // auto res = fn(args...);
-                // storage[arg] = res;
-                // return res;
-                storage.insert(std::make_pair(
-                    arg, fn(args...)
-                ).first->second);
-            } else {
-                return it->second;
-            }
+            std::cout << "returning cached value" << std::endl;
+            
+            return it->second;
         }
-    }
+        std::cout << "first occurence" << std::endl;
+        return cache.insert(std::make_pair(key, std::apply(fun, key))).first->second;
+    };
+}
 }
