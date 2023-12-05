69210d45b065d7757a9779ff140f25232fe68e0e
emdagh <34509796+emdagh@users.noreply.github.com>
2023-12-05 11:20:09 +0100

    Update decorator.hpp

--- include/wee/core/decorator.hpp
+++ include/wee/core/decorator.hpp
@@ -10,13 +10,14 @@ public:
   decorator(std::function<T(Ts...)> f) : _f(f) {}
   T operator () (Ts... ts)
   {
-    return _f(ts);
+    return _f(ts...);
   }
 };
 
 template <typename T, typename... Ts>
 decorator<T(Ts...)> make_decorator(T(*f)(Ts...))
 {
-  return decorator<T(Ts...)>(std::function<R(Args...)>(f));
+  return decorator<T(Ts...)>(std::function<T(Ts...)>(f));
 }
+
 }
