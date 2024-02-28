1153bd29e5afbc88d36a216f6f373f5d63da4858
emdagh <34509796+emdagh@users.noreply.github.com>
2024-02-28 11:59:31 +0100

    Update threadpool.hpp

--- include/wee/core/threadpool.hpp
+++ include/wee/core/threadpool.hpp
@@ -9,90 +9,81 @@
 #include <future>
 #include <functional>
 #include <stdexcept>
-#include <core/singleton.h>
 
 namespace atom {
-
 class threadpool 
-: public singleton<threadpool> 
 {
-    std::vector<std::thread> workers;
-    std::queue<std::function<void()> > tasks;
-    std::mutex queue_mutex;
-    std::condition_variable condition;
-    bool stop;
+    std::vector<std::thread> _workers;
+    std::queue<std::function<void()> > _tasks;
+    std::mutex _mtx;
+    std::condition_variable _cv;
+    bool _stop;
 public:
-    threadpool();
-    template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) 
-        -> std::future<typename std::result_of<F(Args...)>::type>;
-    ~threadpool();
-};
- 
-// the constructor just launches some amount of workers
-inline threadpool::threadpool()
-    :   stop(false)
-{
-    size_t threads = std::thread::hardware_concurrency();
-    for(size_t i = 0;i<threads;++i)
-        workers.emplace_back(
+    threadpool() : _stop(false) 
+    {
+        size_t threads = std::thread::hardware_concurrency();
+        for(size_t i = 0;i < threads;++i)
+            _workers.emplace_back(
             [this]
             {
                 for(;;)
                 {
                     std::function<void()> task;
-
                     {
-                        std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
-                        if(this->stop && this->tasks.empty())
+                        std::unique_lock<std::mutex> lock(_mtx);
+                        _cv.wait(lock,
+                            [this]{ return _stop || !_tasks.empty(); });
+                        if(_stop && _tasks.empty())
                             return;
-                        task = std::move(this->tasks.front());
-                        this->tasks.pop();
+                        task = _tasks.front();
+                        _tasks.pop();
                     }
-
                     task();
                 }
             }
         );
-}
-
-// add new work item to the pool
-template<class F, class... Args>
-auto threadpool::enqueue(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>
-{
-    using return_type = typename std::result_of<F(Args...)>::type;
-
-    auto task = std::make_shared< std::packaged_task<return_type()> >(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
-        
-    std::future<return_type> res = task->get_future();
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-
-        // don't allow enqueueing after stopping the pool
-        if(stop)
-            throw std::runtime_error("enqueue on stopped threadpool");
-
-        tasks.emplace([task](){ (*task)(); });
     }
-    condition.notify_one();
-    return res;
-}
 
-// the destructor joins all threads
-inline threadpool::~threadpool()
-{
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        stop = true;
-    }
-    condition.notify_all();
-    for(std::thread &worker: workers)
+    virtual ~threadpool() {
+        {
+            std::unique_lock<std::mutex> lock(_mtx);
+            _stop = true;
+        }
+        _cv.notify_all();
+        for(std::thread &worker: _workers)
         worker.join();
-}
+    }
 
+    template<class F, class... Args>
+    auto enqueue(F&& f, Args&&... args) { 
+        using return_type = decltype(f(args...));
+
+        auto task = std::make_shared< std::packaged_task<return_type(Args...)> >(
+           std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+        );
+            
+        std::future<return_type> res = task->get_future();
+        {
+            std::unique_lock<std::mutex> lock(_mtx);
+
+            // don't allow enqueueing after stopping the pool
+            if(_stop)
+                throw std::runtime_error("enqueue on stopped threadpool");
+
+            _tasks.emplace([=](){ (*task)(args...); });
+        }
+        _cv.notify_one();
+        return res;
+    }
+};
+
+/*
+int main(int , char**) {
+    threadpool tp;
+    auto res = tp.enqueue([] (int a) {
+        return a * 2;
+    }, 10);
+    
+    return res.get();
+}*/
 }
